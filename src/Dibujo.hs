55ca8cfb0b97018b1f55e4763e2071e1ff9c37b8
Tomi Gonzalia <tomigonzalia@gmail.com>
2023-03-28 16:51:35 -0300

    primeros cambios en dibujo.hs

--- src/Dibujo.hs
+++ src/Dibujo.hs
@@ -17,13 +17,13 @@ Gramática de las figuras:
     | Encimar <Fig> <Fig>
 -}
 
-data Dibujo a =   Borrar
+data Dibujo a =  Borrar
 		| Figura (a)
-		| Rotar (Dibujo a)
+		| Rotar (Dibujo a) -- Rotar 90
 		| Espejar (Dibujo a)
 		| Rot45 (Dibujo a)
 		| Apilar (Float) (Float) (Dibujo a) (Dibujo a)
-		| Juntar (Float) (Float) (Dibujo a) (Dibujo a)  
+		| Juntar (Float) (Float) (Dibujo a) (Dibujo a)
 		| Encimar (Dibujo a) (Dibujo a)
 		deriving (Eq, Show)
 
@@ -31,7 +31,6 @@ data Dibujo a =   Borrar
 
 -- Construcción de dibujo. Abstraen los constructores.
 
---------------------------------------------- No iria, la implementacion seria con gloss en el archivo... interp.hs?
 --figura :: (a -> Dibujo a) -- (???)
 --figura =
 
@@ -52,7 +51,7 @@ data Dibujo a =   Borrar
 
 --encimar :: (Dibujo a) -> (Dibujo a) -> (Dibujo a)
 --encimar =
----------------------------------------------
+
 
 -- Composicion n-veces de una funcion con si misma. Componer 0 veces
 -- es la funcion constante, componer 1 vez es aplicar la funcion 1 vez, etc.
@@ -63,45 +62,56 @@ comp f n a = comp f (n-1) (f a)
 
 -- Rotaciones de múltiplos de 90.
 r180 :: Dibujo a -> Dibujo a
-r180 = comp rotar90 2 a
+r180 fig = comp (rotar (2 fig))
 
 r270 :: Dibujo a -> Dibujo a
-r270 a = comp rotar90 3 a
+r270 fig = comp rotar 3 fig
 
 -- Pone una figura sobre la otra, ambas ocupan el mismo espacio.
 (.-.) :: Dibujo a -> Dibujo a -> Dibujo a
-(.-.) a b = Apilar 100 100 a b
+(.-.) fig1 fig2 = apilar 100 100 fig1 fig2
+-- figura
+-- figura
 
 -- Pone una figura al lado de la otra, ambas ocupan el mismo espacio.
 (///) :: Dibujo a -> Dibujo a -> Dibujo a
-(///) = 
+(///) fig1 fig2 = juntar 100 100 fig1 fig2
+-- figura figura
 
 -- Superpone una figura con otra.
 (^^^) :: Dibujo a -> Dibujo a -> Dibujo a
-(^^^) =
+(^^^) fig1 fig2 = encimar fig1 fig2
+-- ffiigguurraa
 
 -- Dadas cuatro figuras las ubica en los cuatro cuadrantes.
 cuarteto :: Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a
-cuarteto =
+cuarteto fig1 fig2 fig3 fig4 = (///) ((.-.) fig1 fig2) ((.-.) fig3 fig4)
+-- fig1 fig3
+-- fig2 fig4
 
 -- Una figura repetida con las cuatro rotaciones, superpuestas.
 encimar4 :: Dibujo a -> Dibujo a
-encimar4 =
+encimar4 fig = (^^^) ((^^^) (fig) (rotar fig)) ((^^^) (r180 fig) (r270 fig))
 
 -- Cuadrado con la misma figura rotada i * 90, para i ∈ {0, ..., 3}.
 -- No confundir con encimar4!
 ciclar :: Dibujo a -> Dibujo a
-ciclar =
+ciclar fig = cuarteto (fig) (rotar fig) (r180 fig) (r270 fig)
 
--- Ver un a como un dibujo
+-- Ver un a como un dibujo (Casteo?)
 figura :: a -> Dibujo a
-figura =
+figura fig = figura fig
 
 -- map para nuestro lenguaje
-mapDib :: (a -> Dibujo b)-> Dibujo a -> Dibujo b
-mapDib f ? =
-mapDib f a =
-
+mapDib :: (a -> Dibujo b) -> Dibujo a -> Dibujo b
+mapDib func Borrar = Borrar
+mapDib func (Figura fig) = Figura (func fig)
+mapDib func (Rotar fig) = Rotar (mapDib func fig)
+mapDib func (Espejar fig) = Espejar (mapDib func fig)
+mapDib func (Rot45 fig) = Rot45 (mapDib func fig)
+mapDib func (Apilar num1 num2 fig1 fig2) = Apilar num1 num2 (mapDib func fig1) (mapDib func fig2)
+mapDib func (Juntar num1 num2 fig1 fig2) = Juntar num1 num2 (mapDib func fig1) (mapDib func fig2)
+mapDib func (Encimar fig1 fig2) = Encimar (mapDib func fig1) (mapDib func fig2)
 -- Verificar que satisfaga la siguiente igualdad:
 -- mapDib figura = id (id es la funcion identidad)
 
